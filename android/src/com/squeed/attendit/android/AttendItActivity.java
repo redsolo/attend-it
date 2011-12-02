9e30e103723ebaef31d123f6ecaa2e537f9673c5
Erik Ramfelt <eramfelt@gmail.com>
2011-12-02 16:35:41 +0100

    Added filter on text from text box

--- android/src/com/squeed/attendit/android/AttendItActivity.java
+++ android/src/com/squeed/attendit/android/AttendItActivity.java
@@ -7,10 +7,14 @@ import com.squeed.attendit.api.PersonDTO;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 public class AttendItActivity extends ListActivity {
 	private ArrayList<PersonDTO> persons;
 	private PersonAdapter listAdapter;
+	private EditText filterText;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -24,6 +28,9 @@ public class AttendItActivity extends ListActivity {
 		person.setUser("erik.ramfelt");
 		person.setName("Erik Ramfelt");
 		persons.add(person);
+		
+		   filterText = (EditText) findViewById(R.building_list.search_box);
+		    filterText.addTextChangedListener(filterTextWatcher);
 		
 		
 		
@@ -41,5 +48,20 @@ public class AttendItActivity extends ListActivity {
 		m_ProgressDialog = ProgressDialog.show(SoftwarePassionView.this,
 				"Please wait...", "Retrieving data ...", true);*/
 		
+		
+		
 	}
+
+	private TextWatcher filterTextWatcher = new TextWatcher() {
+		@Override
+		public void onTextChanged(CharSequence s, int start, int before, int count) {
+			listAdapter.getFilter().filter(s);
+		}
+		@Override
+		public void afterTextChanged(Editable arg0) {
+		}
+		@Override
+		public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {	
+		}
+	};
 }
--- android/src/com/squeed/attendit/android/PersonAdapter.java
+++ android/src/com/squeed/attendit/android/PersonAdapter.java
@@ -1,30 +1,68 @@
 package com.squeed.attendit.android;
 
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 
 import com.squeed.attendit.api.PersonDTO;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class PersonAdapter extends ArrayAdapter<PersonDTO> {
+public class PersonAdapter extends ArrayAdapter<PersonDTO> implements Filterable {
 
-	private ArrayList<PersonDTO> items;
+	private ArrayList<PersonDTO> persons;
 
 	public PersonAdapter(Context context, int textViewResourceId,
 			ArrayList<PersonDTO> items) {
-		super(context, textViewResourceId, items);
-		this.items = items;
+		super(context, textViewResourceId, new ArrayList<PersonDTO>(items));
+		this.persons = items;
 	}
+	
+	private List<PersonDTO> getFilteredResults(CharSequence constraint) {
+		List<PersonDTO> list = new ArrayList<PersonDTO>();
+		for (PersonDTO person : persons) {
+			if (person.getName().toLowerCase().contains(constraint.toString().toLowerCase())) {
+				list.add(person);
+			}
+		}
+		return list;
+	}
+
+	@Override
+	public Filter getFilter() {
+		return new Filter() {
+			@Override
+			protected void publishResults(CharSequence constraint, FilterResults results) {
+				PersonAdapter.this.clear();
+				for (PersonDTO person : (ArrayList<PersonDTO>) results.values) {
+					PersonAdapter.this.add(person);	
+				}				
+			    PersonAdapter.this.notifyDataSetChanged();
+			}
+		
+			@Override
+			protected FilterResults performFiltering(CharSequence constraint) {
+				List<PersonDTO> filteredResults = getFilteredResults(constraint);
+	            FilterResults results = new FilterResults();
+	            results.values = filteredResults;
+	            return results;
+			}
+	    };
+	}
+
 
 	@Override
     public View getView(int position, View convertView, ViewGroup parent) {
@@ -33,29 +71,42 @@ public class PersonAdapter extends ArrayAdapter<PersonDTO> {
             LayoutInflater vi = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             v = vi.inflate(R.layout.person_list_item, null);
         }
-        PersonDTO o = items.get(position);
+        PersonDTO o = getItem(position);
         if (o != null) {
             TextView tt = (TextView) v.findViewById(R.id.toptext);
             TextView bt = (TextView) v.findViewById(R.id.bottomtext);
             if (tt != null) {
-                  tt.setText("Name: "+o.getName());                            }
+                  tt.setText(o.getName());                            }
             if(bt != null){
-                  bt.setText("Email: "+ o.getEmailAddress());
+                  bt.setText(o.getEmailAddress());
             }
             ImageView iv = (ImageView) v.findViewById(R.id.icon);
-            loadImageFromWebOperations(iv, "http://www.gravatar.com/avatar/" + MD5Util.md5Hex(o.getEmailAddress()));
+            new DownloadImageTask(iv).execute("http://www.gravatar.com/avatar/" + MD5Util.md5Hex(o.getEmailAddress()));
         }
         return v;
     }
+	
+	private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+		
+		private final ImageView view;
 
-	private void loadImageFromWebOperations(ImageView imageView, String url) {
-		try {
-			InputStream is = (InputStream) new URL(url).getContent();
-			Drawable d = Drawable.createFromStream(is, "src name");
-			imageView.setImageDrawable(d);
-		} catch (Exception e) {
-			System.out.println("Exc=" + e);
-			imageView.setImageResource(R.drawable.ic_launcher);
+		public DownloadImageTask(ImageView view) {
+			this.view = view;
+			
 		}
-	}
+	     protected Bitmap doInBackground(String... urls) {
+	    	try {
+	    		Bitmap bitmap = BitmapFactory.decodeStream((InputStream)new URL(urls[0]).getContent());
+	            return bitmap;
+	 		} catch (Exception e) {
+	 			return null;
+	 		}
+	     }
+
+	     protected void onPostExecute(Bitmap result) {
+	    	 if (result != null) {
+	    		 view.setImageBitmap(result);
+	    	 }
+	     }
+	 }
 }
