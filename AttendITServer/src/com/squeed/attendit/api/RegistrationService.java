e0e764c5bfd0e4ef7e12520afa7131260405f152
Erik Lupander <erik.lupander@qmatic.com>
2011-12-02 16:58:25 +0100

    Added RESTEasy integration classes. Created a simple build script, added some simple configuration

--- AttendITServer/src/com/squeed/attendit/api/RegistrationService.java
+++ AttendITServer/src/com/squeed/attendit/api/RegistrationService.java
@@ -2,14 +2,41 @@ package com.squeed.attendit.api;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+@Path("registration")
+@Produces("application/json")
+@Consumes("application/json")
 public interface RegistrationService {
-	public void register(String emailAddress);
+	
+	@POST
+	@Path("/register/{email}")
+	public void register(@PathParam("email") String emailAddress);
+	
+	@POST
+	@Path("/register/attendant/{id}")
 	public void register(Long id);
+	
+	@POST
+	@Path("/unregister/{email}")
 	public void unregister(String emailAddress);
+
+	@POST
+	@Path("/register/attendant/{id}")
 	public void unregister(Long id);
 	
-	public void update(Long id, PersonDTO person);
+	@PUT
+	@Path("/person/{id}")
+	public void update(@PathParam("id") Long id, PersonDTO person);
 	
-	public List<AttendantDTO> getAttendants(Long eventId);
+	@Path("/event/{eventId}/attendants")
+    @GET
+	public List<AttendantDTO> getAttendants(@PathParam("eventId") Long eventId);
 	
 }
