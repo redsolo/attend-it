1e305174d04588d0894c752bd1c51f9bc0566153
Erik Ramfelt <eramfelt@gmail.com>
2011-12-03 10:15:28 +0100

    Added gson to classpath properly

--- android/src/com/squeed/attendit/android/AttendItActivity.java
+++ android/src/com/squeed/attendit/android/AttendItActivity.java
@@ -1,11 +1,19 @@
 package com.squeed.attendit.android;
 
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
 import java.util.ArrayList;
+import java.util.List;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.squeed.attendit.api.AttendantDTO;
 import com.squeed.attendit.api.PersonDTO;
 
 import android.app.Activity;
 import android.app.ListActivity;
+import android.net.http.AndroidHttpClient;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -23,6 +31,20 @@ public class AttendItActivity extends ListActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.person_list);
 
+		
+		RestClient client = new RestClient("http://192.168.43.213:8080/attendit/rest/registration/event/0/attendants");
+		try {
+			client.Execute(RestClient.RequestMethod.GET);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		Type jsonType = new TypeToken<List<AttendantDTO>>(){}.getType();
+		List<AttendantDTO> attendents = new Gson().fromJson(client.getResponse(), jsonType);
+		
+		
+		
+		
 		persons = new ArrayList<PersonDTO>();
 		
 		PersonDTO person = new PersonDTO();
@@ -35,6 +57,8 @@ public class AttendItActivity extends ListActivity {
 		
 		listAdapter = new PersonAdapter(this, R.layout.person_list_item, persons);
 		setListAdapter(listAdapter);
+		
+		
 /*
 		viewOrders = new Runnable() {
 			@Override
